6edf585f99a71b6ecd530410c01dfe770c0bd852
soeun0127 <147484055+soeun0127@users.noreply.github.com>
2024-03-07 00:04:44 +0900

    Title: 369 게임, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240307/369 게임/369-game.py
@@ -0,0 +1,18 @@
+n = int(input())
+i = 1
+
+while i <= n:
+    if i < 10:
+        if i % 3 == 0:
+            print(0, end = " ")
+        elif (i%10) % 3 == 0:
+            print(0, end = " ")
+        else:
+            print(i, end = " ")
+        i += 1
+    else:
+        if (i//10) % 3 == 0:
+            print(0, end = " ")
+        else:
+            print(i, end = " ")
+        i += 1
