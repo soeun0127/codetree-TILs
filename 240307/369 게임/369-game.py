af8b10522f4495c4cb3f642d7c30a018eccee604
soeun0127 <147484055+soeun0127@users.noreply.github.com>
2024-03-07 00:26:30 +0900

    Title: 369 게임, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240307/369 게임/369-game.py
+++ 240307/369 게임/369-game.py
@@ -6,14 +6,10 @@ while i <= n:
         print(0, end = " ")
     elif i % 10 == 0:
         print(i, end = " ")
-    elif (i % 10) % 3 == 0: #1의 자리
-        print(0, end = " ")
     elif i >= 10:
         if (i//10) % 3 == 0: #10의 자리
             print(0, end = " ")
-        elif i % 3 == 0:
-            print(0, end = " ")
-        elif (i%10) % 3 == 0:
+        elif (i%10) % 3 == 0:#1의 자리
             print(0, end = " ")
         else:
             print(i, end = " ")
