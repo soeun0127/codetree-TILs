0f246dbe2de9c7ba5a00a4a782746b5874629772
soeun0127 <147484055+soeun0127@users.noreply.github.com>
2024-03-09 16:40:47 +0900

    Title: 청소당번 정하기, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240309/청소당번 정하기/cleaning-numbering.py
+++ 240309/청소당번 정하기/cleaning-numbering.py
@@ -4,12 +4,12 @@ cntToilet = 0
 
 date = int(input())
 
-for i in range(date+1):
-    if i % 12 == 0 and i != 0:
+for i in range(1, date+1):
+    if i % 12 == 0:
         cntToilet += 1
-    elif i % 3 == 0 and i != 0:
+    elif i % 3 == 0:
         cntHall += 1
-    elif i % 2 == 0 and i != 0:
+    elif i % 2 == 0:
         cntClass += 1
 
 print(cntClass, cntHall, cntToilet)
