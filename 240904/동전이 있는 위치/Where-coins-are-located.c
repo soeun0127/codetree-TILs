9e61186755a1dd3d68f34409d2e4d696c279cf30
soeun0127 <147484055+soeun0127@users.noreply.github.com>
2024-09-04 13:16:22 +0900

    Title: 동전이 있는 위치, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240904/동전이 있는 위치/Where-coins-are-located.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n = 0, m = 0;
+    scanf("%d %d", &n, &m);
+    int arr[81][2] = {0,};
+
+    for(int i=0; i<m; i++){
+        int r = 0, c = 0;
+        scanf("%d %d", &r, &c);
+        arr[r-1][c-1] = 1;
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
